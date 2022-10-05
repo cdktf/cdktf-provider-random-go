be0bc764ab12f6b9cd6a19c4d040afa7ab1db4c6
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-10-05 00:15:55 +0000

    chore(release): v3.0.2

--- random/uuid/uuid_Uuid.go
+++ random/uuid/uuid_Uuid.go
@@ -2,10 +2,10 @@ package uuid
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/hashicorp/cdktf-provider-random-go/random/v3/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-random-go/random/v3/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/hashicorp/cdktf-provider-random-go/random/v3/uuid/internal"
+	"github.com/cdktf/cdktf-provider-random-go/random/v3/uuid/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
