e2973d0cbf6586b6347add185d0898bfe703a418
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-01-17 14:51:18 +0000

    chore(release): v5.0.0

--- random/pet/pet_Pet.go
+++ random/pet/pet_Pet.go
@@ -2,10 +2,10 @@ package pet
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-random-go/random/v4/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-random-go/random/v5/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-random-go/random/v4/pet/internal"
+	"github.com/cdktf/cdktf-provider-random-go/random/v5/pet/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
